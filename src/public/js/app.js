fb5f7ef5f7851d9941138581c2ee40044417d066
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-02-22 01:22:25 +0900

    230222 01:21 learn and create chat with websockets

---
+++ src/public/js/app.js
@@ -0,0 +1,41 @@
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload){
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server ✅")
+})
+
+socket.addEventListener("message", (message)=>{
+    // console.log("New message ", message.data)
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+})
+
+socket.addEventListener("close", ()=>{
+    console.log("Disconnect from Server ❌")
+})
+
+// setTimeout(()=>{
+//     socket.send("hello from the browser!")
+// }, 10000)
+
+messageForm.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    input.value = "";
+})
+nickForm.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    const nick = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", nick.value));
+    input.value = "";
+})
